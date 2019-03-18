1c06b289bffca8b7bd5bb47be2e145670824503c
Gaby <48601948+gabbysocol@users.noreply.github.com>
2019-03-18 09:56:49 +0300

    Update 04-arrays-tasks.js

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -323,7 +323,8 @@ function getPositivesCount(arr) {
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
 function sortDigitNamesByNumericOrder(arr) {
-   throw new Error('Not implemented');
+   let my_dict = {'zero' : 0, 'one' : 1, 'two' : 2, 'three' : 3, 'four' : 4, 'five' : 5, 'six' : 6, 'seven' : 7,  'eight' : 8, 'nine' : 9};
+  	return arr.sort(function(a, b) {return my_dict[a] - my_dict[b]});
 }
 
 /** 
