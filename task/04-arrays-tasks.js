67027f5d17682b64d4400ce5eda235f776c6b767
Gaby <48601948+gabbysocol@users.noreply.github.com>
2019-03-18 11:32:24 +0300

    Update 04-arrays-tasks.js

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -421,7 +421,17 @@ function toStringList(arr) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  */
 function sortCitiesArray(arr) {
-   throw new Error('Not implemented');
+   return arr.sort(function (a, b) {
+     if (a.city < b.city) {
+       return -1;
+     } if (a.city > b.city) {
+         return 1;
+     } return 0;}).sort(function (a, b) {
+     if (a.country < b.country) {
+       return -1;
+     } if (a.country > b.country) {
+         return 1;
+     } return 0;});
 }
 
 /**
