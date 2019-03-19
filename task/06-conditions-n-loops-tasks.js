27f5b49221983267c83e8f618ec692ac963a2dff
Gaby <48601948+gabbysocol@users.noreply.github.com>
2019-03-19 21:32:02 +0300

    Update 06-conditions-n-loops-tasks.js

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -127,7 +127,7 @@ function doRectanglesOverlap(rect1, rect2) {
  * Returns true, if point lies inside the circle, otherwise false.
  * Circle is an object of 
  *  {
- *     center: {
+ *      center: {
  *       x: 5,       
  *       y: 5
  *     },        
@@ -368,7 +368,7 @@ function timespanToHumanString(startDate, endDate) {
  *    365, 10 => '365'
  */
 function toNaryString(num, n) {
-    throw new Error('Not implemented');
+    return num.toString(n);
 }
 
 
@@ -408,7 +408,22 @@ function getCommonDirectoryPath(pathes) {
  *
  */
 function getMatrixProduct(m1, m2) {
-    throw new Error('Not implemented');
+    let rowsM1 = m1.length, colsM1 = m1[0].length,
+        rowsM2 = m2.length, colsM2 = m2[0].length,
+        m3 = [];
+
+    if (colsM1 != rowsM2) 
+	    return false;
+    for (let i = 0; i < rowsM1; i++) 
+	    m3[i] = [];
+    for (let k = 0; k < colsM2; k++) { 
+	    for (let i = 0; i < rowsM1; i++) { 
+		    let temp = 0;          
+		    for (let j = 0; j < rowsM2; j++) 
+			    temp += m1[i][j] * m2[j][k];
+          	    m3[i][k] = temp;
+      	     }
+     } return m3;
 }
 
 
