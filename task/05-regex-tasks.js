d70a9933d76eac97e41926e85cfb9e5cf0cd5468
Gaby <48601948+gabbysocol@users.noreply.github.com>
2019-04-08 00:50:38 +0300

    Update 05-regex-tasks.js

--- task/05-regex-tasks.js
+++ task/05-regex-tasks.js
@@ -117,7 +117,7 @@ function getRegexForSSN() {
  *   'Pa55'.match(validator) => false
  */
 function getPasswordValidator(minLength) {
-   return new RegExp("");
+   return new RegExp("(?!.*[-+_!@#$%^&*.,? ])(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(.+){" + minLength + ",}");
 }
 
 
